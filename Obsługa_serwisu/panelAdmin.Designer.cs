983c187e4fc5351ec1d356941a788de23c628534
tosek <tomaszrosa12345@gmail.com>
2019-06-15 08:42:13 +0200

    last

--- Obsługa_serwisu/panelAdmin.Designer.cs
+++ Obsługa_serwisu/panelAdmin.Designer.cs
@@ -135,7 +135,7 @@
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.label5.Location = new System.Drawing.Point(360, 43);
+            this.label5.Location = new System.Drawing.Point(396, 43);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(160, 24);
             this.label5.TabIndex = 9;
@@ -263,7 +263,7 @@
             // 
             this.label11.AutoSize = true;
             this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.label11.Location = new System.Drawing.Point(36, 258);
+            this.label11.Location = new System.Drawing.Point(56, 258);
             this.label11.Name = "label11";
             this.label11.Size = new System.Drawing.Size(155, 24);
             this.label11.TabIndex = 24;
